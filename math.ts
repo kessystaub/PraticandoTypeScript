931a566fea5f476e693e18374a4593e9f4c8c6f5
kessystaub <51220952+kessystaub@users.noreply.github.com>
2022-08-05 17:50:55 -0300

    Update math.ts

--- math.ts
+++ math.ts
@@ -1,3 +1,15 @@
+class Math_point {
+  x: number;
+  y: number;
+ 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+ 
+const numbers = new Math_point(13, 56);
+
 
 function add(x: number, y: number) {
     x = x+y;
@@ -19,7 +31,9 @@ function mul(x: number, y: number) {
     return x;
 }
 
-console.log("Resultado do add: "+add(1,2));
+console.log("Resultado do add: "+add(numbers.x,numbers.y));
 console.log("Resultado da sub: "+sub(1,2));
 console.log("Resultado da div: "+div(1,2));
 console.log("Resultado da mul: "+mul(1,2));
+
+
